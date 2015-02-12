bb4c6f5bf2d188294f1bd25c1e68341516d0e20c
massivezh <mailregx@yeah.net>
2015-02-12 17:54:34 +0800

    add download range support

--- download-files.go
+++ download-files.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func downloadFromUrl(url string) {
+func downloadFromUrl(url string, start int, end int) {
 	tokens := strings.Split(url, "/")
 	fileName := tokens[len(tokens)-1]
 	fmt.Println("Downloading", url, "to", fileName)
@@ -21,7 +21,16 @@ func downloadFromUrl(url string) {
 	}
 	defer output.Close()
 
-	response, err := http.Get(url)
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	var range_str string
+	range_str = fmt.Sprintf("bytes=%d-%d", start, end)
+	req.Header.Add("Range", range_str)
+
+	response, err := client.Do(req)
+
 	if err != nil {
 		fmt.Println("Error while downloading", url, "-", err)
 		return
@@ -38,9 +47,6 @@ func downloadFromUrl(url string) {
 }
 
 func main() {
-	countries := []string{"GB", "FR", "ES", "DE", "CN", "CA", "ID", "US"}
-	for i := 0; i < len(countries); i++ {
-		url := "http://download.geonames.org/export/dump/" + countries[i] + ".zip"
-		downloadFromUrl(url)
-	}
+	url := "http://mirrors.sohu.com/centos/2/readme.txt"
+	downloadFromUrl(url, 16, 31)
 }
